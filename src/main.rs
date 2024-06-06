cb6617974777060013e0eca215b2bd5788a5e11f
jlnikhilrao <nikhilraojl@outlook.com>
2024-06-06 13:18:50 +0530

    Combine 'bdays' & 'bdays next' output to bdays
    - Remove 'bdays next' option

--- src/main.rs
+++ src/main.rs
@@ -99,100 +99,110 @@ fn main() {
         }
         events.push(e);
     }
-    if let Some(arg) = args.next() {
-        if arg == "next" {
-            find_birthdays_next_7_days(&events)
-        } else {
-            eprintln!("Invalid arguments provided");
-            panic!();
-        }
+    if let Some(_arg) = args.next() {
+        eprintln!("No arguments needed!");
+        return;
     } else {
-        find_birthdays_today(&events)
+        find_birthdays(&events)
     }
 
     // println!("{}", ins_now.elapsed().as_millis());
 }
 
-fn find_birthdays_next_7_days(events: &Vec<Event>) {
-    let now = Local::now();
-    // let curr_day = now.day();
-    // let curr_month = now.month();
-    // let curr_day = 2;
-    // let curr_month = 6;
-
-    let until_date = now + TimeDelta::days(7);
-
-    let mut output_vec: Vec<(i64, &str)> = Vec::new();
-    for event in events {
-        if let Some(Rrule::Yearly(y_rrule)) = &event.rrule {
-            let e_date = Local
-                .with_ymd_and_hms(now.year(), y_rrule.by_month, y_rrule.by_month_day, 0, 0, 0)
-                .unwrap();
-            if now < e_date && e_date < until_date {
-                let e_name = event.summary.as_ref().unwrap();
-                let in_days = e_date - now;
-                output_vec.push((in_days.num_days(), e_name));
-                // let output_str = format!("{} {}", in_days.num_days(), e_name);
-                // output_buf.push_str(&output_str);
-                // output_buf.push('\n');
-            }
-        }
-        // break;
-    }
-
-    if output_vec.is_empty() {
-        println!("No brithdays in upcoming 7 days");
-    } else {
-        let mut max_underline = 5;
-        let mut output_buf = String::new();
-
-        output_vec.sort_by(|a, b| a.0.cmp(&b.0));
-        let mut body_buf = String::new();
-        for e in output_vec {
-            if e.1.len() > max_underline {
-                max_underline = e.1.len();
-            }
-            body_buf.push_str(&format!("{:>2} days | {}", e.0, e.1));
-            body_buf.push('\n');
-        }
-
-        writeln!(output_buf, "{:<7} | {}", " In", "Event").unwrap();
-        writeln!(
-            output_buf,
-            "{}-+-{}",
-            "-".repeat(7),
-            "-".repeat(max_underline)
-        )
-        .unwrap();
-        write!(output_buf, "{}", body_buf).unwrap();
-        println!("{}", output_buf);
-    }
-}
-
-fn find_birthdays_today(events: &Vec<Event>) {
+fn find_birthdays(events: &Vec<Event>) {
     let now = Local::now();
     let curr_day = now.day();
     let curr_month = now.month();
     // let curr_day = 2;
     // let curr_month = 6;
 
-    let mut output_buf = String::new();
+    // let mut output_today = String::new();
+    let mut output_today: Vec<&str> = Vec::new();
+
+    let until_date = now + TimeDelta::days(7);
+    let mut output_7days: Vec<(i64, &str)> = Vec::new();
+
     for event in events {
         if let Some(Rrule::Yearly(y_rrule)) = &event.rrule {
             if y_rrule.by_month == curr_month && y_rrule.by_month_day == curr_day {
                 let e_name = event.summary.as_ref().unwrap();
-                output_buf.push_str("> ");
-                output_buf.push_str(e_name);
-                output_buf.push('\n');
+                output_today.push(e_name);
+            }
+            let e_date = Local
+                .with_ymd_and_hms(now.year(), y_rrule.by_month, y_rrule.by_month_day, 0, 0, 0)
+                .unwrap();
+            if now < e_date && e_date < until_date {
+                let e_name = event.summary.as_ref().unwrap();
+                let in_days = e_date - now;
+                output_7days.push((in_days.num_days(), e_name));
+                // let output_str = format!("{} {}", in_days.num_days(), e_name);
+                // output_buf.push_str(&output_str);
+                // output_buf.push('\n');
             }
         }
     }
 
-    if output_buf.is_empty() {
+    if output_today.is_empty() {
         println!("No brithdays today");
     } else {
-        println!("TODAY");
-        println!("-----");
-        println!("{}", output_buf.trim());
+        let mut max_event_length = 5;
+        let mut output_buf = String::new();
+
+        let mut body_buf_today = String::new();
+        // writeln!(output_buf, "TODAY");
+        // writeln!(output_buf, "-----");
+        for e in &output_today {
+            if e.len() > max_event_length {
+                max_event_length = e.len();
+            }
+            body_buf_today.push_str(&format!("> {}", e));
+            body_buf_today.push('\n');
+        }
+
+        output_7days.sort_by(|a, b| a.0.cmp(&b.0));
+        let mut body_buf_7days = String::new();
+        for e in output_7days {
+            if e.1.len() > max_event_length {
+                max_event_length = e.1.len();
+            }
+            body_buf_7days.push_str(&format!("{:>2} days | {} ", e.0, e.1));
+            body_buf_7days.push('\n');
+        }
+
+        writeln!(output_buf, "-----").unwrap();
+        writeln!(output_buf, "TODAY").unwrap();
+        writeln!(output_buf, "-----").unwrap();
+        writeln!(output_buf, "{}", body_buf_today).unwrap();
+
+        writeln!(
+            output_buf,
+            "{}-+-{}",
+            "-".repeat(7),
+            "-".repeat(max_event_length)
+        )
+        .unwrap();
+        writeln!(
+            output_buf,
+            "{:<7} | {}",
+            " In",
+            " ".repeat(max_event_length)
+        )
+        .unwrap();
+        writeln!(
+            output_buf,
+            "{}-+-{}",
+            "-".repeat(7),
+            "-".repeat(max_event_length)
+        )
+        .unwrap();
+        write!(output_buf, "{}", body_buf_7days).unwrap();
+        writeln!(
+            output_buf,
+            "{}-+-{}",
+            "-".repeat(7),
+            "-".repeat(max_event_length)
+        )
+        .unwrap();
+        println!("{}", output_buf);
     }
 }
